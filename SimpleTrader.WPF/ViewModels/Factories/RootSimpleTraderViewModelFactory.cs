abdbf30e1f728fa24943d6a0bd75d90cb60fa9a4
DennisLu <Dennis2110@hotmail.com.tw>
2022-04-22 16:27:03 +0800

    Part 14: Integrating Authentication with WPF

--- SimpleTrader.WPF/ViewModels/Factories/RootSimpleTraderViewModelFactory.cs
+++ SimpleTrader.WPF/ViewModels/Factories/RootSimpleTraderViewModelFactory.cs
@@ -9,22 +9,26 @@ namespace SimpleTrader.WPF.ViewModels.Factories
     {
         private readonly ISimpleTraderViewModelFactory<HomeViewModel> _homeViewModelFactory;
         private readonly ISimpleTraderViewModelFactory<PortfolioViewModel> _portfolioViewModelFactory;
+        private readonly ISimpleTraderViewModelFactory<LoginViewModel> _loginViewModelFactory;
         private readonly BuyViewModel _buyViewModel;
 
         public RootSimpleTraderViewModelFactory(
             ISimpleTraderViewModelFactory<HomeViewModel> homeViewModelFactory,
             ISimpleTraderViewModelFactory<PortfolioViewModel> portfolioViewModel,
-            BuyViewModel buyViewModel)
+            BuyViewModel buyViewModel, ISimpleTraderViewModelFactory<LoginViewModel> loginViewModelFactory)
         {
             _homeViewModelFactory = homeViewModelFactory;
             _portfolioViewModelFactory = portfolioViewModel;
             _buyViewModel = buyViewModel;
+            _loginViewModelFactory = loginViewModelFactory;
         }
 
         public ViewModelBase CreateViewModel(ViewType viewType)
         {
             switch (viewType)
             {
+                case ViewType.Login:
+                    return _loginViewModelFactory.CreateViewModel();
                 case ViewType.Home:
                     return _homeViewModelFactory.CreateViewModel();
                 case ViewType.Portfolio:
