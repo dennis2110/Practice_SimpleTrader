9c14258b27af96cb40f34a7562f636703ba9d4b3
DennisLu <Dennis2110@hotmail.com.tw>
2022-04-27 10:25:53 +0800

    Part 20: Finishing the home view

--- SimpleTrader.WPF/ViewModels/AssetSummaryViewModel.cs
+++ SimpleTrader.WPF/ViewModels/AssetSummaryViewModel.cs
@@ -10,14 +10,14 @@ namespace SimpleTrader.WPF.ViewModels
     public class AssetSummaryViewModel : ViewModelBase
     {
         private readonly AssetStore _assetStore;
-        private readonly ObservableCollection<AssetViewModel> _assets;
+        private readonly ObservableCollection<AssetViewModel> _topassets;
         public double AccountBalance => _assetStore.AccountBalance;
-        public IEnumerable<AssetViewModel> Assets => _assets;
+        public IEnumerable<AssetViewModel> TopAssets => _topassets;
         public AssetSummaryViewModel(AssetStore assetStore)
         {
             _assetStore = assetStore;
 
-            _assets = new ObservableCollection<AssetViewModel>();
+            _topassets = new ObservableCollection<AssetViewModel>();
 
             _assetStore.StateChanged += AssetStore_StateChanged;
 
@@ -29,11 +29,13 @@ namespace SimpleTrader.WPF.ViewModels
             IEnumerable<AssetViewModel> assetViewModels = _assetStore.AssetTransactions
                 .GroupBy(t => t.Asset.Symbol)
                 .Select(g => new AssetViewModel(g.Key, g.Sum(a => a.IsPurchase ? a.Shares : -a.Shares)))
-                .Where(a => a.Shares > 0);
-            _assets.Clear();
+                .Where(a => a.Shares > 0)
+                .OrderByDescending(a => a.Shares)
+                .Take(3);
+            _topassets.Clear();
             foreach(AssetViewModel viewModel in assetViewModels)
             {
-                _assets.Add(viewModel);
+                _topassets.Add(viewModel);
             }
 
         }
